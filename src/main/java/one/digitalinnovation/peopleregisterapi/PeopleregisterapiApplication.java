e77d850414db938c082cf7c2afbcc1ef9e57f56c
paleblueweb <paleblue.web@outlook.com>
2021-06-28 20:34:06 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/peopleregisterapi/PeopleregisterapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.peopleregisterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleregisterapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleregisterapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/peopleregisterapi/PeopleregisterapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.peopleregisterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleregisterapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
